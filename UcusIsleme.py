3526dd6acf889ba6c0e219e5ba7eb66701780847
Woooper <muratteksinn@gmail.com>
2021-10-11 16:08:36 +0300

    Dıs + Ic Tamanlandı

--- UcusIsleme.py
+++ UcusIsleme.py
@@ -5,8 +5,6 @@ import matplotlib.pyplot as plt
 import time
 import Distance
 
-area_inner=0
-area_outter=0
 
 # Görüntü içe aktarım
 
@@ -14,16 +12,15 @@ path = './Ucus6m.mp4'
 cap = cv2.VideoCapture(path)
 frame_number = 1
 
+
+
+
 target1 = Distance.Target()
 camera2  = Distance.Camera(76,32)
 print(camera2.CamFov)
 mgp     = Distance.Plane()
 mgp.setAltitude(60)
 target1.MeasureDistance(1,camera2,mgp.getAltitude())
-
-
- 
-
 
 while True:
     ret, frame = cap.read()
@@ -42,18 +39,25 @@ while True:
         greenUpper=(70,255,255)
         frame_green_area = cv2.inRange(frame_hsv,greenLower,greenUpper)
         
+        # Canny Kenar bulma
+        cThr=[100,100]
+        img_canny = cv2.Canny(frame_green_area,cThr[0],cThr[1])
+        kernel = np.ones((3,3))
+        
+        
         # Dış Bölge Erozyon ve Genişletme
         
-        frame_erode = cv2.erode(frame_green_area,None,iterations=2)
-        frame_dilate = cv2.dilate(frame_erode,None,iterations=2)
+        frame_dilate = cv2.dilate(img_canny,kernel,iterations=1)
+        frame_erode = cv2.erode(frame_dilate,kernel,iterations=3)
+        
         
 
         # Dış Bölge işaretleme
-        (contours,hierarcy) = cv2.findContours(frame_dilate.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+        (frame_contour,contours,hierarcy) = cv2.findContours(frame_dilate.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+        
         if len(contours)>0:
-            
+
             c = max(contours,key=cv2.contourArea)
-            
             
             rect = cv2.minAreaRect(c)
             
@@ -73,7 +77,7 @@ while True:
             
             ret_gray, threshed_gray= cv2.threshold(target_area, 54, 255, cv2.THRESH_BINARY_INV)
             
-            (contours_gray,hierarcy_gray) = cv2.findContours(threshed_gray.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+            (frame_contour_gray,contours_gray,hierarcy_gray) = cv2.findContours(threshed_gray.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
             
                 
             if len(contours_gray)>0:
@@ -90,29 +94,31 @@ while True:
                 box_gray = np.int64(box_gray)
                 
                 M_gray = cv2.moments(c_gray)
-
-                area_inner = cv2.contourArea(c_gray)
-                area_outer = cv2.contourArea(c)-area_inner
-
                 try:
                     center_gray = (int(M_gray['m10']/M_gray['m00']),int(M_gray['m01']/M_gray['m00']))
+                    center_gray_in_frame = (center_gray[0]+up,center_gray[1]+left)
                 except:
-                    continue
+                    pass
+                
                 cv2.drawContours(target_area,[box_gray], 0, (255,0,255),1)
+                area_inner = cv2.contourArea(c_gray)
                 
                 
-                rect_gray_in_image = ((float(up+center_gray[0]),float(left+center_gray[1])),(w_gray,h_gray),rotation_gray)
                 try:
+                
+                    rect_gray_in_image = ((up+center_gray[0],left+[center_gray[1]]),(w_gray,h_gray),rotation_gray)
                     box_gray_in_image = cv2.boxPoints(rect_gray_in_image)
                     box_gray_in_image_int = np.int64(box_gray_in_image)
                     cv2.drawContours(frame_copy,[box_gray_in_image_int], 0, (255,0,255),1)
+                    cv2.putText(frame_copy, ' | Dis Alan: {}  | Ic Alan: {} | {}'.format(area_outer,area_inner,frame_number),(10,40),cv2.FONT_HERSHEY_TRIPLEX,0.5,(255,255,225),1)
+                    cv2.circle(frame_copy,center_gray_in_frame,1,(255,255,255),2)
                 except:
                     pass
             
                         
-            cv2.putText(frame_copy, ' | Dis Alan: {}  | Ic Alan: {} | {}'.format(area_outer,area_inner,frame_number),(10,40),cv2.FONT_HERSHEY_TRIPLEX,0.5,(255,255,225),1)
-            cv2.circle(frame_copy,center_gray,1,(100,100,0),2)
-
+            
+            
+            
 
             cv2.imshow('Tespit',frame_copy)
             cv2.imshow('Tespit Yesil',frame_dilate)
@@ -127,4 +133,5 @@ while True:
     else : break
 cap.release()
 cv2.destroyAllWindows()       
+        
         
